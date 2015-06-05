1f53df603efbfc7024a7a3d08535b6106d5cdb03
vronvali <vronvali@cesine.ca>
2015-06-05 12:37:17 -0400

    Website for Rock Paper Scissors Game
    Added buttons and colors to make the game “cuter”. Changed original
    code to allow for the results to show, and for the computer choice to
    also be shown to the player. Changed the prompt code to a select box.

--- games/rock_paper_scissors/rock_paper_scissors.js
+++ games/rock_paper_scissors/rock_paper_scissors.js
@@ -6,9 +6,10 @@
  *
  * @return {[type]}
  */
-var RockPaperScissorsGame = function() {
+var RockPaperScissorsGame = function(element) {
   this.run = function() {
-    var userChoice = prompt("Do you choose rock, paper or scissors?");
+    var userChoice = $(element).find("select").val().toLowerCase();
+    // prompt("Do you choose rock, paper or scissors?");
     var computerChoice = Math.random();
     if (computerChoice < 0.34) {
       computerChoice = "rock";
@@ -18,7 +19,10 @@ var RockPaperScissorsGame = function() {
       computerChoice = "scissors";
     }
     console.log("Computer: " + computerChoice);
-    this.compare(userChoice, computerChoice);
+    $(element).find(".computers-choice").text(computerChoice);
+
+    var result = this.compare(userChoice, computerChoice);
+    $(element).find(".result").text(result);
   };
 
   this.compare = function(choice1, choice2) {
